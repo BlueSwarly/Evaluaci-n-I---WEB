5b4bd3a630344acb3a48a8cf837c9c10c0185800
BlueSwarly <00067425@uca.edu.sv>
2026-09-15 23:15:23 -0600

    :rocket: Feat: Validaciones de incidencuasControler.js

---
+++ controllers/incidenciasController.js
@@ -0,0 +1,37 @@
+const { generarId } = require('../utils/helpers');
+
+const CrearIncidencia = (req, res) => {
+
+    const {empleado, area, descripcion, prioridad } = req.body;
+    const nuevoPaquete = {empleado, area, descripcion, prioridad };
+
+    if(!empleado || !area || !descripcion ||!prioridad){
+        return res.status(400).json({ error: 'Faltan datos obligatorios'}) 
+        //te falto tirarle datos al sistema
+    }
+
+    //Vamos a hacer una validacion con .trim() para evitar que se visaree el sistema
+    //por espacios en blanco
+    else if(empleado.trim().length===0|| area.trim().length===0|| descripcion.trim().length===0|| prioridad.trim().length===0){
+        return res.status(400).json({ error: 'No se aceptan espacios vacios'}) 
+    }
+
+    //Validacion para evitar que el usuario se ponga muchas mas o menos prioridad de la que
+    //en realidad existe
+    else if(prioridad !== "Alta" && prioridad !== "Media" && prioridad !== "Baja"){
+        return res.status(400).json({ error: 'Tipo de prioridad no valida'}) 
+    }
+
+    const nuevaIncidencia = {
+        id: generarId(), //Generamos una identificacion para el objeto
+        empleado: empleado.trim(),
+        area: area.trim(),
+        descripcion: descripcion.trim(),
+        prioridad: prioridad.trim(), 
+        estado: "Pendiente" //Asignamos un estado por defecto al objeto
+    };
+
+    incidencias.push(nuevaIncidencia);
+
+    res.status(400).json({ mensaje: "Incidencia registrada correctamente" });
+}
